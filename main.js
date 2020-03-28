7971264916794444dd8306765aa6b1b834f426dc
Robert Danitz <robert.danitz@here.com>
2020-03-28 14:34:08 +0100

    use raleway font; simplify

--- main.js
+++ main.js
@@ -8,8 +8,7 @@ const mountApp = riot.component(App)
 
 const app = mountApp(
     document.getElementById('root'),
-    { welcome: "<div class=\"me\">Anna Sundukova</div><h2>Experience & Service Designer bringing the methodologies of UX and human-centred design to the design of spaces.</h2><p>Also a product strategist, design thinking consultant, workshop facilitator, public speaker – let’s embrace the complexity!<br/>To request portfolio, ask a question or simply get in touch, contact me:<br/><a href=\"mailto:anna@annasundukova.com\">anna@annasundukova.com</a></p><p>[full website coming soon]</p>",
-      me: {
+    { me: {
           email: 'anna@annasundukova.com',
           pinterest: 'annasundukova',
           linkedin: 'annasundukova',
